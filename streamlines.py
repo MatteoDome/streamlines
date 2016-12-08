34b39791ad044bf0ce46821db7c2c29db248cf0e
Matteo Domenighini <Matteo Domenighini>
2016-12-08 14:05:15 +0100

    clean directory and color code velocity

--- streamlines.py
+++ streamlines.py
@@ -1,58 +1,95 @@
-import os
-os.chdir("vtk_data\dissection")
- 
 import vtk
+import os
+#os.chdir("vtk_data\dissection")
+os.chdir("vtk_data\synthetic")
+
+#filename = "metrics_07.vti"
+filename = "image_03.vti"
  
-pl3d = vtk.vtkMultiBlockPLOT3DReader()
- 
-xyx_file = "metrics_07.vti"
-pl3d.SetXYZFileName(xyx_file)
-pl3d.SetScalarFunctionNumber(100)
-pl3d.SetVectorFunctionNumber(202)
-pl3d.Update()
- 
+#read file
+reader = vtk.vtkXMLImageDataReader()
+reader.SetFileName(filename)
+reader.Update()
+output = reader.GetOutput()
+
+# Get vectors
+velocity_field = output.GetPointData().GetArray(0)
+output.GetPointData().SetVectors(velocity_field);
+print("Number of velocity vector components: "+str(velocity_field.GetNumberOfComponents()))
+
+#get additional info from the file
+origin = output.GetOrigin()
+print(origin)
+origin_x = origin[0]
+origin_y = origin[1]
+origin_z = origin[2]
+
+extent = output.GetExtent()
+print(extent)
+xmin = extent[0]+origin_x
+xmax = extent[1]+origin_x
+ymin = extent[2]+origin_y
+ymax = extent[3]+origin_y
+zmin = extent[4]+origin_z
+zmax = extent[5]+origin_z
+
+center_x = xmin + (xmax-xmin)/2
+center_y = ymin + (ymax-ymin)/2
+center_z = zmin + (zmax-zmin)/2
+
+#"seeds" defines points where the streamlines are seeded:
 seeds = vtk.vtkPlaneSource()
-seeds.SetXResolution(4)
-seeds.SetYResolution(4)
-seeds.SetOrigin(2,-2,26)
-seeds.SetPoint1(2, 2,26)
-seeds.SetPoint2(2,-2,32)
+seeds.SetXResolution(8)
+seeds.SetYResolution(8)
+seeds.SetOrigin(center_x, center_y, center_z)
+seeds.SetPoint1(center_x+20, center_y, center_z)
+seeds.SetPoint2(center_x, center_y+20, center_z)
  
+#basic vtk stream line filter
 streamline = vtk.vtkStreamLine()
-streamline.SetInputData(pl3d.GetOutput().GetBlock(0))
-streamline.SetSourceConnection(seeds.GetOutputPort())
+streamline.SetInput(output)
+streamline.SetSource(seeds.GetOutput())
 streamline.SetMaximumPropagationTime(200)
 streamline.SetIntegrationStepLength(.2)
 streamline.SetStepLength(0.001)
 streamline.SetNumberOfThreads(1)
 streamline.SetIntegrationDirectionToForward()
 streamline.VorticityOn()
+streamline.SpeedScalarsOn()
+
+streamTube = vtk.vtkTubeFilter()
+streamTube.SetInputConnection(streamline.GetOutputPort())
+streamTube.SetInputArrayToProcess(1, 0, 0, vtk.vtkDataObject.FIELD_ASSOCIATION_POINTS, "vectors")
+streamTube.SetRadius(0.2)
+streamTube.SetNumberOfSides(12)
+streamTube.SetVaryRadiusToVaryRadiusByVector()
+mapStreamTube = vtk.vtkPolyDataMapper()
+mapStreamTube.SetInputConnection(streamTube.GetOutputPort())
+streamTubeActor = vtk.vtkActor()
+streamTubeActor.SetMapper(mapStreamTube)
+streamTubeActor.GetProperty().BackfaceCullingOn()
  
+#streamline mapper
 streamline_mapper = vtk.vtkPolyDataMapper()
 streamline_mapper.SetInputConnection(streamline.GetOutputPort())
+
+#streamline actor
 streamline_actor = vtk.vtkActor()
 streamline_actor.SetMapper(streamline_mapper)
-streamline_actor.VisibilityOn()
- 
-outline = vtk.vtkStructuredGridOutlineFilter()
-outline.SetInputData(pl3d.GetOutput().GetBlock(0))
-outline_mapper = vtk.vtkPolyDataMapper()
-outline_mapper.SetInputConnection(outline.GetOutputPort())
-outline_actor = vtk.vtkActor()
-outline_actor.SetMapper(outline_mapper)
-outline_actor.GetProperty().SetColor(1,1,1)
- 
+# streamline_actor.VisibilityOn()
+
+# Setup rendering
 renderer = vtk.vtkRenderer()
-render_window = vtk.vtkRenderWindow()
-render_window.AddRenderer(renderer)
-interactor = vtk.vtkRenderWindowInteractor()
-interactor.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
-render_window.SetInteractor(interactor)
+
+renderer.AddActor(streamTubeActor)
+renderer.ResetCamera()
+renderer.SetBackground(0.7,.7,1);
  
-renderer.AddActor(streamline_actor)
-renderer.AddActor(outline_actor)
+renderWindow = vtk.vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
  
-renderer.SetBackground(0.1, 0.2, 0.4)
-interactor.Initialize()
-render_window.Render()
-interactor.Start()
+renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+ 
+renderWindowInteractor.SetRenderWindow(renderWindow)
+renderWindowInteractor.Initialize()
+renderWindowInteractor.Start()
